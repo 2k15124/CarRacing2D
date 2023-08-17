61abb5205b192bd4cc8dbe15aa03bf1f13e8ef8b
Abdullah3435 <ak08428@st.habib.edu.pk>
2023-08-17 17:14:14 +0500

    Script changes

--- Assets/githubTestScript.cs
+++ Assets/githubTestScript.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class githubTestScript : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Version 1");
+        Debug.Log("Test bu Abd");
     }
 
     // Update is called once per frame
